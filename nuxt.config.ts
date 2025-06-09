0b5b180368825ebadda5cc5efeb10fbae5b18194
NhatNT <nhatnt1@nal.vn>
2025-06-09 14:35:05 +0700

    config: alias @ to root folder

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,41 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import Aura from '@primeuix/themes/aura'
+import Aura from "@primeuix/themes/aura";
+import path from "path";
 
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', // '@nuxt/eslint',
-  '@primevue/nuxt-module', '@pinia/nuxt'],
-  css: [
-    'primeicons/primeicons.css',
-    '~/assets/css/tailwind.css'
+  vite: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname)
+      }
+    }
+  },
+  modules: [
+    "@nuxtjs/tailwindcss", // '@nuxt/eslint',
+    "@primevue/nuxt-module",
+    "@pinia/nuxt"
   ],
+  css: ["primeicons/primeicons.css", "~/assets/css/tailwind.css"],
   devtools: { enabled: true },
-  compatibilityDate: '2025-05-15',
+  compatibilityDate: "2025-05-15",
   eslint: {
     config: {
       stylistic: true,
-      checked: true,
-    },
+      checked: true
+    }
   },
   primevue: {
     options: {
       theme: {
-        preset: Aura,
-      },
+        preset: Aura
+      }
     },
     autoImport: true,
     components: {
-      include: '*',
-    },
+      include: "*"
+    }
   },
   tailwindcss: {
     // Options
-  },
-})
+  }
+});
